8485c8c0224dae5200d552603e1ecbca8c788355
CJ Ellis <cellis@talentpath.com>
2019-11-16 22:23:12 -0600

    Moved server to raspberry pi

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 // Initialize and add the map
 let map;
-const url = "http://localhost:3200";
+const url = "http://99.26.184.205:4243";
 let heldAddresses = {};
 let markerArray = [];
 let markerLatLngArray = [];

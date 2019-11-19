fa106d41be8a394c662ead17eef10cc2f673b1f0
CJ Ellis <cellis@talentpath.com>
2019-11-19 16:30:15 -0600

    slowed requests down so all show up

--- map.js
+++ map.js
@@ -1,9 +1,11 @@
 // Initialize and add the map
 let map;
-const url = "http://99.26.184.205:4243";
+const url = "http://99.26.184.205:4243"; //"http://localhost:4243"
 let heldAddresses = {};
 let markerArray = [];
 let markerLatLngArray = [];
+let go = true;
+let counter = 10;
 
 function initMap() {
   var geoC = new google.maps.Geocoder();
@@ -14,9 +16,9 @@ function initMap() {
         address: address
       },
       function(results, status) {
-        if (status == "OK") {
+        if (status === "OK") {
           map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 11,
+            zoom: 8,
             center: results[0].geometry.location,
             styles: styles
           });
@@ -32,15 +34,25 @@ function initMap() {
   // Let google map load
   new google.maps.event.addDomListener(window, "load", getData);
 
-  // Pull addresses every 15 seconds, onedrive updates every minute
+  // Pull addresses every 10 seconds, onedrive updates every minute
+
+  setInterval(() => {
+    go = true;
+  }, 50);
+
   setInterval(() => {
     getData();
-  }, 15000);
+  }, 1000);
 }
 
 getData = () => {
   getEverything().then(addresses => {
-    addMarker(addresses);
+    for (id in addresses) {
+      if (heldAddresses[id] === undefined && go) {
+        addMarker(id, addresses);
+        go = false;
+      }
+    }
   });
 };
 
@@ -57,113 +69,99 @@ async function getEverything() {
   }
 }
 
-addMarker = addressArray => {
+addMarker = (id, addressArray) => {
   let geoC = new google.maps.Geocoder();
-  for (const address in addressArray) {
-    geoC.geocode(
-      {
-        address: address
-      },
-      function(results, status) {
-        const id = addressArray[address].idValue;
-        const formattedHeld = results[0].formatted_address;
-        const inside = addressArray[address].insideDMGValue;
-        const parking = addressArray[address].parkingDMGValue;
+  geoC.geocode(
+    {
+      address: addressArray[id].addressValue
+    },
+    function(results, status) {
+      console.log(results[0].formatted_address);
+      const formattedHeld = results[0].formatted_address;
+      const inside = addressArray[id].insideDMGValue;
+      const parking = addressArray[id].parkingDMGValue;
+      heldAddresses[id] = {
+        formattedHeld,
+        inside,
+        parking
+      };
 
-        if (heldAddresses[id] === undefined) {
-          heldAddresses[id] = {
-            formattedHeld,
-            inside,
-            parking
+      let totalInside = 0;
+      let totalParking = 0;
+      let totalFlood = 0;
+
+      for (const id in heldAddresses) {
+        if (heldAddresses[id].formattedHeld === results[0].formatted_address) {
+          totalInside += heldAddresses[id].inside;
+          totalParking += heldAddresses[id].parking;
+        }
+      }
+      totalFlood = totalInside + totalParking;
+
+      if (status === "OK") {
+        let image;
+
+        if (totalFlood < 0) {
+          image = {
+            url: "./good.png",
+            scaledSize: new google.maps.Size(25, 25)
+          };
+        } else if (totalFlood > 0) {
+          image = {
+            url: "./bad.png",
+            scaledSize: new google.maps.Size(25, 25)
+          };
+        } else if (totalFlood === 0) {
+          image = {
+            url: "./okay.png",
+            scaledSize: new google.maps.Size(25, 25)
           };
         }
 
-        let totalInside = 0;
-        let totalParking = 0;
-        let totalFlood = 0;
+        if (
+          isLocationFree([
+            results[0].geometry.location.lat(),
+            results[0].geometry.location.lng()
+          ])
+        ) {
+          var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            icon: image,
+            animation: google.maps.Animation.NONE
+          });
+          markerArray.push(marker);
+          markerLatLngArray.push([
+            results[0].geometry.location.lat(),
+            results[0].geometry.location.lng()
+          ]);
 
-        for (const id in heldAddresses) {
-          if (
-            heldAddresses[id].formattedHeld === results[0].formatted_address
-          ) {
-            totalInside += heldAddresses[id].inside;
-            totalParking += heldAddresses[id].parking;
-          }
-        }
-        totalFlood = totalInside + totalParking;
+          const infowindow = new google.maps.InfoWindow({
+            content: results[0].formatted_address
+          });
 
-        if (status == "OK") {
-          let image;
-
-          if (totalFlood < 0) {
-            image = {
-              url: "./good.png",
-              scaledSize: new google.maps.Size(25, 25)
-            };
-          } else if (totalFlood > 0) {
-            image = {
-              url: "./bad.png",
-              scaledSize: new google.maps.Size(25, 25)
-            };
-          } else if (totalFlood === 0) {
-            image = {
-              url: "./okay.png",
-              scaledSize: new google.maps.Size(25, 25)
-            };
-          }
-
-          if (
-            isLocationFree([
-              results[0].geometry.location.lat(),
-              results[0].geometry.location.lng()
-            ])
-          ) {
-            var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location,
-              icon: image,
-              animation: google.maps.Animation.NONE
-            });
-            markerArray.push(marker);
-            markerLatLngArray.push([
-              results[0].geometry.location.lat(),
-              results[0].geometry.location.lng()
-            ]);
-
-            const infowindow = new google.maps.InfoWindow({
-              content: results[0].formatted_address
-            });
-
-            marker.addListener("click", function() {
-              infowindow.open(map, marker);
-            });
-          } else {
-            for (i = 0; i < markerArray.length; i++) {
-              if (
-                markerArray[i].position.lat() ===
-                  results[0].geometry.location.lat() &&
-                markerArray[i].position.lng() ===
-                  results[0].geometry.location.lng() &&
-                image.url !== markerArray[i].getIcon().url
-              ) {
-                console.log(
-                  "marker's current icon: " + markerArray[0].getIcon().url
-                );
-                console.log("marker's new icon: " + image.url);
-                console.log(
-                  results[0].formatted_address + " is changing icon!"
-                );
-                markerArray[i].setIcon(image);
-              }
+          marker.addListener("click", function() {
+            infowindow.open(map, marker);
+          });
+        } else {
+          for (i = 0; i < markerArray.length; i++) {
+            if (
+              markerArray[i].position.lat() ===
+                results[0].geometry.location.lat() &&
+              markerArray[i].position.lng() ===
+                results[0].geometry.location.lng() &&
+              image.url !== markerArray[i].getIcon().url
+            ) {
+              markerArray[i].setIcon(image);
             }
           }
-        } else {
-          console.log("This didnt work: " + status);
-          return null;
         }
+      } else {
+        console.log("This didnt work: " + status);
+        return null;
       }
-    );
-  }
+    }
+  );
 };
 
 isLocationFree = LatLng => {

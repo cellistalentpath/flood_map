0ec700ec131a582cbfddb17822f66ecd22b73244
CJ Ellis <cellis@talentpath.com>
2019-12-06 08:43:17 -0600

    fixing margins

--- map.js
+++ map.js
@@ -394,7 +394,7 @@ function changeLocation(location) {
 
 function resetHeight() {
   // reset the body height to that of the inner browser
-  document.body.style.height = window.innerHeight + "px";
+  document.body.style.height = `${window.innerHeight}px`;
 }
 // reset the height whenever the window's resized
 window.addEventListener("resize", resetHeight);

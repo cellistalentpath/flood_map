b23fa466d8dc968806124681339fce440a7ec686
CJ Ellis <cellis@talentpath.com>
2020-02-17 20:16:52 -0600

    fixed heroku address

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 let map;
 var geoC;
-const url = "http://localhost:4243";
+const url = "flood-map42.herokuapp.com";
 let heldAddresses = {};
 let markerArray = [];
 let infoWindowArray = [];
